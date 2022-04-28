d109f9f8210b977dfc4c70ad72729f4d287428d6
Jorge Guillen <jguillenm1500@alumno.ipn.mx>
2022-04-28 00:59:45 -0500

    Se instanciaron 3 objetos en el index del proyecto

---
+++ index.js
@@ -0,0 +1,8 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander('Carlo');
+const rodrigo = new MissionCommander('Rodrigo');
+const fernanda = new MissionCommander('Fernanda');
+
+console.log(carlo.name);
+console.log(rodrigo.name);
+console.log(fernanda.name);
